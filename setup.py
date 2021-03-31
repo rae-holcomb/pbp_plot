711f23857c7f6e109c2bd8ab8f12f6d9be8fb08e
rae-holcomb <69921202+rae-holcomb@users.noreply.github.com>
2021-03-31 12:10:48 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="pbp_plot",
+    version="0.0.1",
+    author="Rae Holcomb",
+    author_email="raeholcomb19@gmail.com",
+    description="Create pixel-by-pixel plots from TESS full frame images.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pbp_plot/homepage/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
