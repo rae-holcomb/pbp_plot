b0fbc3e7ca04223bb62d73462189623f444d8f9e
rae-holcomb <69921202+rae-holcomb@users.noreply.github.com>
2021-07-22 11:30:02 -0700

    Update pbp_plot_demo.py
    added comments

--- pbp_plot_demo.py
+++ pbp_plot_demo.py
@@ -11,12 +11,16 @@ target_name = 'LSPM J1835+3259'
 tpf = pbp_plot.download_tpf(target_name)
 
 # Set the paramters to plot a light curve
+plot_type = 'lc'
 x_lim = [2016.75,2017.5]	# time range in BJD - 2457000 (BTJD)
-padding = 1
-pbp_plot.plot_pixel_by_pixel(target_name, tpf, plot_type='lc', padding=1, x_lim=x_lim, save=True)
+padding = 1					# the number of pixels outside of the TESS aperture you would like plotted
+							# Note: a large padding can take longer to plot
+pbp_plot.plot_pixel_by_pixel(target_name, tpf, plot_type=plot_type, padding=padding, x_lim=x_lim, save=True)
 
 
 # Set the paramters to plot a periodogram
+plot_type = 'periodogram'
 period_lim = [.5,4.]	# range of periods in units of days
-padding = 1
-pbp_plot.plot_pixel_by_pixel(target_name, tpf, plot_type='periodogram', padding=1, x_lim=period_lim, save=True)
+padding = 1				# the number of pixels outside of the TESS aperture you would like plotted
+						# Note: a large padding can take longer to plot
+pbp_plot.plot_pixel_by_pixel(target_name, tpf, plot_type=plot_type, padding=padding, x_lim=period_lim, save=True)
